4daece8f5e65ca6813ad99b6e1f1e582f8a44c40
Toyash <toyashpatil17@gmail.com>
2025-04-20 20:41:51 +0530

    feat:twilio Test

--- routes/Whatsapp.js
+++ routes/Whatsapp.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/checkHealth',(req,res)=>{
-    res.json({
-        message:"Health ok"
-    })
-})
+const { MessagingResponse } = require('twilio').twiml;
 
 
+router.get('/checkHealth', (req, res) => {
+  res.json({
+    message: "Health ok"
+  });
+});
+
+
+router.post('/incoming', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  
+  const userMessage = req.body.Body;
+  console.log('User said:', userMessage);
+
+ 
+  twiml.message('Hello from Nurapix');
+
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
+});
 
 module.exports = router;
